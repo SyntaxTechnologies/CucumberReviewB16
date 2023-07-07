2e83633297d54cc10c5788aa5a7d4ff89974603e
moazzamsadiq <moazzam@syntaxtechs.com>
2023-07-06 20:20:40 -0400

    232143

--- src/test/java/runners/FailedRunner.java
+++ src/test/java/runners/FailedRunner.java
@@ -6,23 +6,22 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        //in failed runner just 2 things are required, features and glue
         //features we use to provide the path of all the feature files
-        features = "src/test/resources/features/",
+        features = "@target/failed.txt",
         //glue keyword we use to provide the package where step defs are available
         glue = "steps",
         //when dry run is true, it stops actual execution, it quickly scans all the steps and will
         //provide the missing step definition
-        dryRun = false,
-        tags="@sprint1",
-
+        // dryRun = false,
+        // tags = "@login1",
         //it means sometimes the console output for cucumber test is having some
         //irrelevant information, when you set it to true, it removes all that
         //irrelevant information from the console and will give you simple output
-        monochrome = true,
+        //  monochrome = true,
         //it used to print all the steps in console
-        plugin = {"pretty","html:target/cucumber.html","json:target/cucumber.json","rerun:target/failed.txt" }
+        //html plugin is generating the report, this report will be generated under target folder
+        plugin = {"pretty"}
 )
-
-public class RunnerClass {
-
+public class FailedRunner {
 }
